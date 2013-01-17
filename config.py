c76ed9adcac50439928a8d70cd8ee99dae6ff155
jatchili <jecatripple@gmail.com>
2013-01-17 15:12:23 -0800

    added config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+# Replace these with the appropriate value for your own case.
+
+rpcuser="bitcoinrpc"
+rpcpassword="218qBhMbD6HK5hJJ2Kmgk3GbJ7DUrnZy5oNmZeJgjFSn"
+
+RIPPLE_WEBSOCKET_URL = 'wss://s1.ripple.com:51233'
+MY_RIPPLE_ADDRESS = 'rsWMoLZhRqTGsmztMRyv5UrE28bTbn8gAH'
+MY_SECRET_KEY = 'ssoLaaRpFGZgnj5QoE95yuqbLXcMb'
